b8871f6c3e86b64613344a86b9b838af32b8c2b1
Carlos Rodriguez <carlos@s8f.org>
2012-08-05 16:58:15 -0700

    closePort has a bug :-/

--- bin/instrument.js
+++ bin/instrument.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
 var synth = require('../')();
-
 console.log('virtual MIDI instrument open on port "%s"', synth.portName);
-
-process.on('SIGINT', synth.closePort);
-process.on('SIGTERM', synth.closePort);
