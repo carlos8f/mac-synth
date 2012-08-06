7bbcbc7e6f283b5887b539663350e1100c8c47a1
Carlos Rodriguez <carlos@s8f.org>
2012-08-05 20:17:46 -0700

    add --verbose option

--- bin/instrument.js
+++ bin/instrument.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
-var synth = require('../')();
+var argv = require('optimist')
+    .alias('v', 'verbose')
+    .argv
+  , synth = require('../')()
+
 console.log('virtual MIDI instrument open on port "%s"', synth.portName);
+
+if (argv.verbose) {
+  synth.on('message', function(deltaTime, message) {
+    console.log('received message: ' + message);
+  });
+}
